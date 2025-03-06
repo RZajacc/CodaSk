4f7cc3bf19f177dfc6528c5fd0221d80734b411f
Rafal Zajac <rf.zajac@gmail.com>
2025-03-06 12:30:29 +0100

    removed unnecessary wrappers on home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,58 +1,53 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import helloGif from '../../public/ezgif.com-video-to-gif.gif';
 import BoxLink from '@/components/Ui/links/BoxLink';
 
 export default async function page() {
   return (
-    <main className="h-full ">
-      <div>
-        <div className="my-5 grid justify-items-center">
-          <Image
-            className=" rounded-lg"
-            width={900}
-            height={900}
-            alt="homepage-photo"
-            src={helloGif}
-          />
-          <h1 className="lg:text-6x m-1 pb-8 pt-6 text-center font-bold text-[#6741D9] md:text-3xl">
-            Welcome to Codask,
-            <br />a learning platform and community <br />
-            for Codac students and alumnis!
-          </h1>
+    <div className="my-5 grid justify-items-center">
+      <Image
+        className=" rounded-lg"
+        width={900}
+        height={900}
+        alt="homepage-photo"
+        src={helloGif}
+      />
+      <h1 className="lg:text-6x m-1 pb-8 pt-6 text-center font-bold text-[#6741D9] md:text-3xl">
+        Welcome to Codask,
+        <br />a learning platform and community <br />
+        for Codac students and alumnis!
+      </h1>
 
-          <div className="flex min-w-full flex-col justify-center">
-            <div
-              id="purpleCTAcontainer"
-              className="rounded-lg bg-[#B197FC] p-10 pb-10"
-            >
-              <h2 className="mb-4 text-center text-3xl font-semibold dark:text-white">
-                Stuck on a problem? Let us help...
-              </h2>
+      <div className="flex min-w-full flex-col justify-center">
+        <div
+          id="purpleCTAcontainer"
+          className="rounded-lg bg-[#B197FC] p-10 pb-10"
+        >
+          <h2 className="mb-4 text-center text-3xl font-semibold dark:text-white">
+            Stuck on a problem? Let us help...
+          </h2>
 
-              <div className="flex flex-row justify-around">
-                <BoxLink url="/search/questions">Search by questions</BoxLink>
-                <BoxLink url="/search/tags"> Search by tags</BoxLink>
-                <BoxLink url="/search/modules">Search by modules</BoxLink>
-              </div>
-            </div>
+          <div className="flex flex-row justify-around">
+            <BoxLink url="/search/questions">Search by questions</BoxLink>
+            <BoxLink url="/search/tags"> Search by tags</BoxLink>
+            <BoxLink url="/search/modules">Search by modules</BoxLink>
           </div>
-
-          <Image
-            className="mainLogo my-3 items-center"
-            src={'/CodaskLogo.png'}
-            alt="Logo"
-            width={210}
-            height={210}
-          />
-
-          <pre>
-            <code className=" text-[#E91E63]">
-              Happy learning and happy coding!
-            </code>
-          </pre>
         </div>
       </div>
-    </main>
+
+      <Image
+        className="mainLogo my-3 items-center"
+        src={'/CodaskLogo.png'}
+        alt="Logo"
+        width={210}
+        height={210}
+      />
+
+      <pre>
+        <code className=" text-[#E91E63]">
+          Happy learning and happy coding!
+        </code>
+      </pre>
+    </div>
   );
 }
