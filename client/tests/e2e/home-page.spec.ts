a9d13cc0fe60ff6793b35039a9dcd902cdcdf3f0
Rafal Zajac <rf.zajac@gmail.com>
2025-03-06 12:01:50 +0100

    added few simple e2e tests to home page, testing its navigation

--- client/tests/e2e/home-page.spec.ts
+++ client/tests/e2e/home-page.spec.ts
@@ -1,12 +1,36 @@
 import {test, expect} from '@playwright/test';
 
-test('should navigate to questions page', async ({page}) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('/');
+test.describe('Home page navigation', () => {
+  test('should navigate to questions page', async ({page}) => {
+    // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+    await page.goto('/');
 
-  // Find an element with the text 'About' and click on it
-  await page.click('text=Search by questions');
+    // Find an element with the text 'About' and click on it
+    await page.getByRole('link', {name: 'Search by questions'}).click();
 
-  // The new URL should be "/about" (baseURL is used there)
-  await expect(page).toHaveURL('/search/questions');
+    // The new URL should be "/about" (baseURL is used there)
+    await expect(page).toHaveURL('/search/questions');
+  });
+
+  test('should navigate to tags page', async ({page}) => {
+    // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+    await page.goto('/');
+
+    // Find an element with the text 'About' and click on it
+    await page.getByRole('link', {name: 'Search by tags'}).click();
+
+    // The new URL should be "/about" (baseURL is used there)
+    await expect(page).toHaveURL('/search/tags');
+  });
+
+  test('should navigate to modules page', async ({page}) => {
+    // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+    await page.goto('/');
+
+    // Find an element with the text 'About' and click on it
+    await page.getByRole('link', {name: 'Search by modules'}).click();
+
+    // The new URL should be "/about" (baseURL is used there)
+    await expect(page).toHaveURL('/search/modules');
+  });
 });
