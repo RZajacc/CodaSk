09551a092c1905202cfbe35280f31f58f6b52e96
Rafal Zajac <rf.zajac@gmail.com>
2026-01-13 15:16:34 +0100

    Feature: Add TS to backend
    - Also removed graphQL entirely
    - Routes need to be restored still

---
+++ server/src/server.ts
@@ -0,0 +1,67 @@
+import express from "express";
+import mongoose from "mongoose";
+import colors from "colors";
+import cors from "cors";
+import * as dotenv from "dotenv";
+// import userRoutes from "../routes/userRoutes.js"
+// import questionRoutes from "../routes/questionRoutes.js";
+// import answerRoutes from "../routes/answerRoute.js";
+// import cloudinaryConfig from "../config/cloudinaryConfig.js";
+
+
+dotenv.config();
+
+
+
+const app = express();
+
+const addMiddlewares = () => {
+  app.use(express.json());
+
+  app.use(cors({
+    origin: [process.env.LOCAL_URL!,
+      process.env.SERVICE_NAME!,
+      process.env.PUBLIC_URL!,
+      process.env.PUBLIC_DNS!,],
+  }));
+  
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  // cloudinaryConfig();
+};
+
+const addRoutes = () => {
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  })
+  // app.use("/api/users", userRoutes);
+  // app.use("/api/questions", questionRoutes);
+  // app.use("/api/answers", answerRoutes);
+};
+
+const DBConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB!);
+    console.log("connection to MongoDB successful :>> ".bgCyan);
+  } catch (error) {
+    console.log("error in DBConnection:>> ".bgRed, error);
+  }
+};
+
+const startServer = async () => {
+  const port = process.env.PORT || 5008;
+
+  app.listen(port, () => {
+    console.log("Server running in port:".bgGreen, port);
+  });
+};
+
+(async function controller() {
+  await DBConnection();
+  addMiddlewares();
+  addRoutes();
+  await startServer();
+})();
