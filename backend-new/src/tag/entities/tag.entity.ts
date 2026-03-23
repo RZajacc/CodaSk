6ca37aea278147a159463fd2eabc764f0f48290a
Rafal Zajac <rf.zajac@gmail.com>
2026-03-23 14:37:03 +0100

    Implement tag entity

--- backend-new/src/tag/entities/tag.entity.ts
+++ backend-new/src/tag/entities/tag.entity.ts
@@ -1,1 +1,25 @@
-export class Tag {}
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Question } from '../../question/entities/question.entity';
+
+export type TagDocument = HydratedDocument<Tag>;
+
+@Schema()
+export class Tag {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+    required: false,
+  })
+  related_questions: Question[];
+
+  @Prop({ required: true })
+  course_type: string;
+}
+
+export const TagSchema = SchemaFactory.createForClass(Tag);
