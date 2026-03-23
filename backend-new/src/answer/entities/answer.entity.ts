55cbe1e0a64163e9f7641f5fcb91f3dc52c7f180
Rafal Zajac <rf.zajac@gmail.com>
2026-03-23 14:29:45 +0100

    Build answer entity

--- backend-new/src/answer/entities/answer.entity.ts
+++ backend-new/src/answer/entities/answer.entity.ts
@@ -1,1 +1,33 @@
-export class Answer {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from '../../user/entities/user.entity';
+import { Question } from '../../question/entities/question.entity';
+
+export type AnswerDocument = HydratedDocument<Answer>;
+
+@Schema()
+export class Answer {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  author: User;
+
+  @Prop({ required: true })
+  posted_on: Date;
+
+  @Prop({ required: true })
+  message: string;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Question',
+    required: true,
+  })
+  question: Question;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    required: false,
+  })
+  votes: User[];
+}
+
+export const AnswerSchema = SchemaFactory.createForClass(Answer);
