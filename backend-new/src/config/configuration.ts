04906fd631053fe63babf487754e0812ca071009
Rafal Zajac <rf.zajac@gmail.com>
2026-03-24 14:56:59 +0100

    Add cors configuration

--- backend-new/src/config/configuration.ts
+++ backend-new/src/config/configuration.ts
@@ -1,6 +1,7 @@
 export default () => ({
   app: {
     port: parseInt(process.env.PORT || '5000', 10),
+    origins: process.env.CORS_ORIGINS?.split(',') || [],
   },
   database: {
     mongoURI: process.env.MONGO_URI,
--- backend-new/src/main.ts
+++ backend-new/src/main.ts
@@ -5,12 +5,23 @@ import { ConfigService } from '@nestjs/config';
 interface AppConfig {
   app: {
     port: number;
+    origins: string[];
   };
 }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const configService = app.get(ConfigService<AppConfig, true>);
+
+  const allowedOrigins = configService.get('app.origins', { infer: true });
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    withCredentials: true,
+  });
 
   await app.listen(configService.get('app.port', { infer: true }));
 }
