e57d8adde07de0321a0575ff4fae25acd19a7505
Rafal Zajac <rf.zajac@gmail.com>
2026-04-13 15:04:45 +0200

    Implement prettier, also to tailwind

--- frontendNew/src/App.tsx
+++ frontendNew/src/App.tsx
@@ -1,29 +1,29 @@
-import {createBrowserRouter, RouterProvider, type RouteObject} from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  type RouteObject,
+} from 'react-router';
 
 function App() {
+  const routes: RouteObject[] = [
+    {
+      path: '/',
+      element: (
+        <div className={'p-1 text-3xl font-bold'}>Hello from router</div>
+      ),
+    },
+  ];
 
+  const routes2: RouteObject[] = [
+    {
+      path: '/test',
+      element: <div>Hello from test</div>,
+    },
+  ];
 
-    const routes: RouteObject[] = [
-        {
-            path: '/',
-            element: <div>Hello from router</div>
-        }
-    ]
+  const router = createBrowserRouter([...routes, ...routes2]);
 
-    const routes2: RouteObject[] = [
-        {
-            path: '/test',
-            element: <div >Hello from test</div>,
-        }
-    ]
-
-    const router = createBrowserRouter(
-        [...routes, ...routes2]
-    )
-
-    return (
-        <RouterProvider router={router}/>
-    )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
