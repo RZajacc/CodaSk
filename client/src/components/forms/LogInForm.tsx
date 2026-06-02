0fbd62e0122a8d8b37db53e44ea780317ae608f9
Rafal Zajac <rf.zajac@gmail.com>
2026-06-02 15:11:08 +0200

    Organise Login component code

--- client/src/components/forms/LogInForm.tsx
+++ client/src/components/forms/LogInForm.tsx
@@ -1,73 +1,46 @@
 import FormInput from '../../components/Ui/Inputs/FormInput';
-import {useAuth} from '../../context/AuthContext.tsx';
-import {useNavigate} from 'react-router';
-import {useForm, type SubmitHandler, FormProvider} from 'react-hook-form';
-import {zodResolver} from '@hookform/resolvers/zod';
-import {type LoginInputType, LoginSchema} from '../../schemas/AuthSchemas.ts';
+import {type SubmitHandler, useFormContext} from 'react-hook-form';
+import {type LoginInputType} from '../../schemas/AuthSchemas.ts';
 
-export default function LogInForm() {
-  const {login, error} = useAuth();
-  const navigate = useNavigate();
+type Props = {
+  onSubmit: SubmitHandler<LoginInputType>;
+  loginError: string | null;
+};
 
-  const methods = useForm<LoginInputType>({
-    resolver: zodResolver(LoginSchema),
-  });
-
-  const {handleSubmit} = methods;
-
-  const onSubmit: SubmitHandler<LoginInputType> = async (data) => {
-    try {
-      const user = await login(data.email, data.password);
-      navigate(`/user/profile/${user._id}`);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export default function LogInForm({onSubmit, loginError}: Props) {
+  const {handleSubmit} = useFormContext<LoginInputType>();
 
   return (
-    <div className="w-96 rounded-2xl bg-[#EDE9E6] p-10">
-      <FormProvider {...methods}>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          data-testid="login-form"
-          className="mb-4 grid gap-3"
-        >
-          <FormInput
-            label={'Email'}
-            htmlFor={'email'}
-            type={'email'}
-            placeholder={'email'}
-            required={true}
-          />
-          <FormInput
-            label={'Password'}
-            htmlFor={'password'}
-            type={'password'}
-            placeholder={'password'}
-            required={true}
-          />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      data-testid="login-form"
+      className="mb-4 grid w-96 gap-3 rounded-2xl bg-[#EDE9E6] p-6"
+    >
+      <FormInput
+        label={'Email'}
+        htmlFor={'email'}
+        type={'email'}
+        placeholder={'email'}
+        required={true}
+      />
+      <FormInput
+        label={'Password'}
+        htmlFor={'password'}
+        type={'password'}
+        placeholder={'password'}
+        required={true}
+      />
 
-          <button
-            type="submit"
-            className="rounded-full bg-black px-4 py-2 font-bold text-white hover:bg-[#B197FC]"
-          >
-            log in
-          </button>
+      <button
+        type="submit"
+        className="mb-4 rounded-full bg-black px-4 py-2 font-bold text-white hover:bg-[#B197FC]"
+      >
+        log in
+      </button>
 
-          {error && <div className={'text-center text-red-500'}>{error}</div>}
-        </form>
-      </FormProvider>
-      {/*<button*/}
-      {/*  onClick={() => {*/}
-      {/*    signIn('github', {*/}
-      {/*      redirect: false,*/}
-      {/*    });*/}
-      {/*  }}*/}
-      {/*  className="rounded border-b-4 border-[#D9D9D9] bg-[#6741D9] px-4 py-2 font-bold text-white hover:border-black hover:bg-[#9AFF80] hover:text-black"*/}
-      {/*>*/}
-      {/*  <FaGithub style={{fontSize: '2em'}} />*/}
-      {/*  log in with Github*/}
-      {/*</button>*/}
-    </div>
+      {loginError && (
+        <div className={'text-center text-red-500'}>{loginError}</div>
+      )}
+    </form>
   );
 }
--- client/src/pages/user/login/index.tsx
+++ client/src/pages/user/login/index.tsx
@@ -1,12 +1,41 @@
 import LogInForm from '../../../components/forms/LogInForm';
-import {Link} from 'react-router';
+import {Link, useNavigate} from 'react-router';
+import {useAuth} from '../../../context/AuthContext.tsx';
+import {FormProvider, type SubmitHandler, useForm} from 'react-hook-form';
+import {
+  type LoginInputType,
+  LoginSchema,
+} from '../../../schemas/AuthSchemas.ts';
+import {zodResolver} from '@hookform/resolvers/zod';
 
 export default function Login() {
+  const {login, error} = useAuth();
+  const navigate = useNavigate();
+
+  const methods = useForm<LoginInputType>({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  const {reset} = methods;
+
+  const onSubmit: SubmitHandler<LoginInputType> = async (data) => {
+    try {
+      const user = await login(data.email, data.password);
+      navigate(`/user/profile/${user._id}`);
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="h-full bg-[#6741D9]">
+      {/* TITLE */}
       <h1 className="p-4 text-center text-xl font-medium text-white md:text-3xl">
         Please log in:
       </h1>
+
+      {/* BOX CONTAINING FORM AND OPTIONAL IMAGE */}
       <div className="grid justify-center">
         <div className="relative z-20">
           <img
@@ -18,7 +47,13 @@ export default function Login() {
               'https://res.cloudinary.com/dfm1r4ikr/image/upload/v1701535913/codask/website_photos/pngtree-element-abstract-yellow-pastel-blob-png-image_6568008_l2qiel.png'
             }
           />
-          <LogInForm />
+
+          {/* FORM WITH A FORM WRAPPER */}
+          <FormProvider {...methods}>
+            <LogInForm onSubmit={onSubmit} loginError={error} />
+          </FormProvider>
+
+          {/* LINK TO REGISTER FORM */}
           <p className="m-4 text-center font-medium text-white">
             <Link to={'/user/register'}>
               Don't have an account yet? sign up!
