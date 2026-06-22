0b6e429387ae7ddd1911e4d060dbfebe1284801a
Rafal Zajac <rf.zajac@gmail.com>
2026-06-22 16:33:14 +0200

    Add react form hook

--- client/src/components/forms/SignUpForm.tsx
+++ client/src/components/forms/SignUpForm.tsx
@@ -1,110 +1,17 @@
-import {type User} from '../../types/custom_types';
-import {type FormEvent, useEffect, useState} from 'react';
-// import {BuildFetchUrl} from '@/utils/BuildFetchUrl';
-import FormInput from '../../components/Ui/Inputs/FormInput';
+import FormInput from '../Ui/Inputs/FormInput.tsx';
+import {type SubmitHandler, useFormContext} from 'react-hook-form';
+import type {LoginInputType} from '../../schemas/AuthSchemas.ts';
 
-function SignUpForm() {
-  // Build Fetch url
-  // const FETCH_URL = BuildFetchUrl();
-  const FETCH_URL = 'http://localhost:3000/';
+type Props = {
+  onSubmit: SubmitHandler<any>;
+};
 
-  const [newUser, setNewUser] = useState<User>({
-    first_name: '',
-    last_name: '',
-    email: '',
-    password: '',
-    user_photo:
-      'https://res.cloudinary.com/dfm1r4ikr/image/upload/v1701685725/codask/website_photos/user_photo_default.png',
-    bio: '',
-    location: {
-      city: '',
-      country: '',
-    },
-    course_type: '',
-    course_date: Date(),
-    cohort_name: '',
-    user_permission: '',
-    website: '',
-    github: '',
-    member_since: new Date(),
-    last_seen: Date(),
-    questions: [],
-    answers: [],
-    saved_tags: [],
-  });
-
-  const handleRegister = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    // * Get the form data
-    const formData = new FormData(e.currentTarget);
-    const email = formData.get('email') as string;
-    const password = formData.get('password') as string;
-
-    if (email && password) {
-      if (!email.includes('@') && password.length < 6) {
-        alert(
-          'Your email seems to be invalid. \n Your password should be at least 6 characters'
-        );
-        return;
-      } else if (password.length < 6) {
-        alert('Your password should be at least 6 characters');
-        return;
-      } else if (!email.includes('@')) {
-        alert('Your email seems to be invalid');
-        return;
-      }
-
-      try {
-        const myHeaders = new Headers();
-        myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
-
-        const urlencoded = new URLSearchParams();
-
-        urlencoded.append('email', newUser.email);
-        urlencoded.append('password', newUser.password);
-        urlencoded.append('user_photo', newUser.user_photo);
-
-        const requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: urlencoded,
-        };
-
-        try {
-          const response = await fetch(
-            `${FETCH_URL}/api/users/signup`,
-            requestOptions
-          );
-          if (response.ok) {
-            const result = await response.json();
-            // console.log('result in register:>> ', result);
-
-            setNewUser(result);
-            alert('Thank you for signing up!  🤓');
-            // await signIn('credentials', {
-            //   ...newUser,
-            //   redirect: false,
-            // });
-          }
-          // router.push('/user/moreinfo');
-          // location.reload();
-        } catch (error) {
-          console.log('error in your /signup fetch:>> ', error);
-        }
-      } catch (error) {
-        console.log('error when adding new user:>> ', error);
-      }
-    }
-  };
-
-  useEffect(() => {
-    // setNewUser(newUser);
-  }, []);
+function SignUpForm({onSubmit}: Props) {
+  const {handleSubmit} = useFormContext<LoginInputType>();
 
   return (
     <div className="w-96 rounded-2xl bg-[#EDE9E6] p-10">
-      <form onSubmit={handleRegister} className="mb-4 grid gap-3">
+      <form onSubmit={handleSubmit(onSubmit)} className="mb-4 grid gap-3">
         <FormInput
           label={'Email'}
           htmlFor={'email'}
@@ -119,6 +26,13 @@ function SignUpForm() {
           placeholder={'password'}
           required={true}
         />
+        <FormInput
+          label={'Confirm Password'}
+          htmlFor={'password'}
+          type={'password'}
+          placeholder={'password'}
+          required={true}
+        />
         <button
           type="submit"
           className="rounded-full bg-black px-4 py-2 font-bold text-white hover:bg-[#B197FC]"
@@ -126,18 +40,6 @@ function SignUpForm() {
           sign up
         </button>
       </form>
-
-      {/*<button*/}
-      {/*  onClick={() => {*/}
-      {/*    // signIn('github', {*/}
-      {/*    //   redirect: false,*/}
-      {/*    // });*/}
-      {/*  }}*/}
-      {/*  className="w-max rounded border-b-4 border-[#D9D9D9] bg-[#6741D9] px-4 py-2 font-bold text-white hover:border-black hover:bg-[#9AFF80] hover:text-black"*/}
-      {/*>*/}
-      {/*  <FaGithub style={{fontSize: '2em'}} />*/}
-      {/*  sign up with Github*/}
-      {/*</button>*/}
     </div>
   );
 }
--- client/src/pages/user/register/index.tsx
+++ client/src/pages/user/register/index.tsx
@@ -1,7 +1,79 @@
 import SignUpForm from '../../../components/forms/SignUpForm';
 import {Link} from 'react-router';
+import {FormProvider, type SubmitHandler, useForm} from 'react-hook-form';
 
 export default function Register() {
+  //   user_photo:
+  //     'https://res.cloudinary.com/dfm1r4ikr/image/upload/v1701685725/codask/website_photos/user_photo_default.png'
+
+  const methods = useForm();
+
+  // const {reset} = methods;
+
+  const onSubmit: SubmitHandler<any> = async (data) => {
+    console.log(data);
+    // * Get the form data
+    // const formData = new FormData(e.currentTarget);
+    // const email = formData.get('email') as string;
+    // const password = formData.get('password') as string;
+    //
+    // if (email && password) {
+    //   if (!email.includes('@') && password.length < 6) {
+    //     alert(
+    //       'Your email seems to be invalid. \n Your password should be at least 6 characters'
+    //     );
+    //     return;
+    //   } else if (password.length < 6) {
+    //     alert('Your password should be at least 6 characters');
+    //     return;
+    //   } else if (!email.includes('@')) {
+    //     alert('Your email seems to be invalid');
+    //     return;
+    //   }
+    //
+    //   try {
+    //     const myHeaders = new Headers();
+    //     myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+    //
+    //     const urlencoded = new URLSearchParams();
+    //
+    //     urlencoded.append('email', newUser.email);
+    //     urlencoded.append('password', newUser.password);
+    //     urlencoded.append('user_photo', newUser.user_photo);
+    //
+    //     const requestOptions = {
+    //       method: 'POST',
+    //       headers: myHeaders,
+    //       body: urlencoded,
+    //     };
+    //
+    //     try {
+    //       const response = await fetch(
+    //         `${FETCH_URL}/api/users/signup`,
+    //         requestOptions
+    //       );
+    //       if (response.ok) {
+    //         const result = await response.json();
+    //         // console.log('result in register:>> ', result);
+    //
+    //         setNewUser(result);
+    //         alert('Thank you for signing up!  🤓');
+    //         // await signIn('credentials', {
+    //         //   ...newUser,
+    //         //   redirect: false,
+    //         // });
+    //       }
+    //       // router.push('/user/moreinfo');
+    //       // location.reload();
+    //     } catch (error) {
+    //       console.log('error in your /signup fetch:>> ', error);
+    //     }
+    //   } catch (error) {
+    //     console.log('error when adding new user:>> ', error);
+    //   }
+    // }
+  };
+
   return (
     <div className="h-full bg-[#6741D9]">
       <h1 className="p-4 text-center text-xl font-medium text-white md:text-3xl">
@@ -18,7 +90,9 @@ export default function Register() {
               'https://res.cloudinary.com/dfm1r4ikr/image/upload/v1701533894/codask/website_photos/pngtree-element-green-blob-png-image_6578665_zxdtvn.png'
             }
           />
-          <SignUpForm />
+          <FormProvider {...methods}>
+            <SignUpForm onSubmit={onSubmit} />
+          </FormProvider>
           <p className="m-4 text-center font-medium text-white">
             <Link to={'/user/login'}>Already have an account? log in!</Link>
           </p>
